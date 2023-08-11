8d6ee661e70ac943dda98b90a32301dbbe6e81c3
Enock Keya <enockkeya@EnockMBP.local>
2023-08-11 11:50:34 +0300

    added a notebook

---
+++ configs.py
@@ -0,0 +1,13 @@
+source_config = {
+    'user': 'username',
+    'password': 'password',
+    'host': 'host-url',
+    'database': 'dbase'
+}
+
+dest_config = {
+    'user': 'username',
+    'password': 'password',
+    'host': 'host-url',
+    'database': 'dbase'
+}
